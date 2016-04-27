9e5f595cfb8ae46a2ad1da3a443aeef3fd0b557a
tsauliu <caoliu1994@gmail.com>
2016-04-27 14:25:24 +0800

    debug

--- analysisdata-BAT&Fround.py
+++ analysisdata-BAT&Fround.py
@@ -8,14 +8,14 @@ stage=["C轮", "D轮", "E轮"]
 # stage=["E轮"]
 i=0
 for data in reader:
-    if data[4]=='IPO上市' or data[4]=="F轮-上市前"\
-            or any(i in BAT for i in data[6:]):# and i<2000:
+
+    if data[4]=='IPO上市' or data[4]=="F轮-上市前":
         #print data[4].decode("gbk"),data[1].decode("gbk")
         madeitlist.append(data[1])
-    #i=i+1
-    if any(i in BAT for i in data[6:]) and data[4] in stage:
-        # print data[4].decode("gbk"),data[1].decode("gbk")
-        madeitlist.append(data[1])
+
+    # if any(i in BAT for i in data[6:]) and data[4] in stage:
+    #     # print data[4].decode("gbk"),data[1].decode("gbk")
+    #     madeitlist.append(data[1])
 
 madeitlist = list(set(madeitlist))#delete mutiples
 
@@ -35,6 +35,7 @@ for company in madeitlist:
             invests.append(line)
     Madeitinvest.update({company:invests})
 
+count=0
 with open("madeitinvests.csv", "wb") as f3:
         for company in Madeitinvest:
             for invest in Madeitinvest[company]:
@@ -43,4 +44,17 @@ with open("madeitinvests.csv", "wb") as f3:
                         # print fund
                 #     # fundlist.append([invest[0],invest[1],fund])
                         f3.write(invest[0]+","+invest[1]+","+invest[4]+","+fund+'\n')
+                        count=count+1
 f3.close()
+print count
+#
+# #all invests
+# import csv
+# comlst=[]
+# reader = csv.reader(open('VCdataHMK.csv', 'rU'))
+# with open("madeitinvests.csv", "wb") as f4:
+#     for data in reader:
+#         for fund in data[6:]:
+#             if not fund=="":
+#                 f4.write(data[0]+","+data[1]+","+data[4]+","+fund+'\n')
+# f4.close()
